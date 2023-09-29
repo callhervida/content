38a9e62186289188c26d8dc3538236cd2f3e07a5
callhervida <vidaa.g1999@gmail.com>
2023-09-29 16:01:39 +0330

    added blog's post model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    """A Model for blog post's information including content's title, content
+    author and Publication Date"""
+
+    title = models.CharField(blank=True, null=True, max_length=100)
+
+    content = models.TextField(blank=True, null=True)
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    created_at = models.DateTimeField(default=timezone.now, blank=True, null=True, verbose_name="تاریخ ایجاد")
