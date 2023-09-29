25c0ae178213e30967b5e92d2cab361921fa6d68
callhervida <vidaa.g1999@gmail.com>
2023-09-29 21:05:57 +0330

    added post list

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
 
+from . views import NewPost, PostList
+
 
 urlpatterns = [
     path('login/', obtain_auth_token, name='login'),
+    path('new-post', NewPost.as_view()),
+    path('post-list', PostList.as_view())
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,60 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
 
-# Create your views here.
+from .models import Post
+from . serializers import PostSerializer
+
+
+class NewPost(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated, ]
+
+    def post(self, request):
+
+        author_id = request.user.id
+
+        title = request.data.get('title')
+
+        content = request.data.get('content')
+
+        data = {
+            'title': title,
+            'content': content,
+            'author': author_id
+        }
+
+        post_serialized = PostSerializer(data)
+
+        if not post_serialized.is_valid():
+            return Response(
+                {
+                    'status': False,
+                    'message': post_serialized.errors,
+                    'data': []
+                },
+                status=200
+            )
+        post_serialized.save()
+
+
+class PostList(APIView):
+
+    def post(self, request):
+
+        post_obj = Post.objects.all()
+
+        post_serialized = PostSerializer(post_obj, many=True)
+
+        return Response(
+            {
+                'status': True,
+                'message': 'success',
+                'data': post_serialized.data
+            },
+            status=200
+        )
+
+
+
